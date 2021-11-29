f10a46cca272f92a1e0ea61f0ed2a0bd4fcea3be
Samuel Oyewole <findseunoyewole@gmail.com>
2021-11-29 11:39:25 -0800

    scroll, navbar

--- script.js
+++ script.js
@@ -19,3 +19,66 @@ navToggle.addEventListener('click', () => {
         linkContainer.style.height  = 0
     }
 }) 
+
+const navbar = document.querySelector('#nav')
+const topLink = document.querySelector('.top-link')
+
+
+window.addEventListener('scroll', () => {
+    const scrollHeigth = window.pageYOffset
+    const navHeight = navbar.getBoundingClientRect().height
+
+    if(scrollHeigth > navHeight){
+        navbar.classList.add('fixed-nav')
+    }
+
+    else{
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if(scrollHeigth > 400){
+        topLink.classList.add('show-link')
+    }
+    else{
+        topLink.classList.remove('show-link')
+    }
+
+
+})
+
+
+const scrollLinks = document.querySelectorAll('.scroll-link')
+scrollLinks.forEach(scrollLink => {
+    scrollLink.addEventListener('click', (e) => {
+        e.preventDefault()
+        
+        const id = e.currentTarget.getAttribute('href').slice(1)
+        const element = document.getElementById(id)
+
+        const container = linkContainer.getBoundingClientRect().height
+        const fixedNav = navbar.classList.contains('fixed-nav')
+
+        const navHeight = navbar.getBoundingClientRect().height
+
+        let position = element.offsetTop - navHeight
+
+        if(!fixedNav){
+            position = position - navHeight
+        }
+
+        if(navHeight > 82){
+            position = position + container
+        }
+      
+        window.scrollTo({
+            left:0,
+            top:position,
+        })
+
+        linkContainer.style.height = 0
+    })
+})
+
+
+
+// about page
